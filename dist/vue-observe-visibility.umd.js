c420a5ce10ba34ab943837e72c10853ad49017c2
Kevin Bon <kevin.bon@fiducial.net>
2019-04-26 09:13:01 +0200

    build version

--- dist/vue-observe-visibility.umd.js
+++ dist/vue-observe-visibility.umd.js
@@ -304,7 +304,9 @@ var VisibilityState = function () {
 
 			// Wait for the element to be in document
 			vnode.context.$nextTick(function () {
-				_this.observer.observe(_this.el);
+				if (_this.observer) {
+					_this.observer.observe(_this.el);
+				}
 			});
 		}
 	}, {
